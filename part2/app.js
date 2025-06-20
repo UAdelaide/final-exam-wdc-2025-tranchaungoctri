517e7cf921fb227d68cb551c6a6623208ef17d06
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 12:38:34 +0000

    [automatic commit] 2025-06-20 12:38:34 UTC

--- part2/app.js
+++ part2/app.js
@@ -11,9 +11,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/auth');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api', authRoutes); //
+
 
 // Export the app instead of listening here
 module.exports = app;
