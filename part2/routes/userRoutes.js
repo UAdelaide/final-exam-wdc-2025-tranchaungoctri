ddbb1b07df639beda8321ce9b614e6015ee936ae
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 13:16:31 +0000

    [automatic commit] 2025-06-20 13:16:31 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -25,7 +25,7 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Registration failed' });
   }
-});
+);
 
 router.get('/me', (req, res) => {
   if (!req.session.user) {
