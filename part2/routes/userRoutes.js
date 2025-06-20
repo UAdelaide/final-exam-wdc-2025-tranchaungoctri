4524c9053ffd9f78eb61e38359c400f6b0f2c265
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 12:48:10 +0000

    [automatic commit] 2025-06-20 12:48:10 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,12 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // Save user info to session
+    // Save user info in session after login
+    req.session.user = {
+      id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
+    };
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
