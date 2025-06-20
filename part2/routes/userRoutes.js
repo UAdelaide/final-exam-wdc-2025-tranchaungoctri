7179ce267f9938fa61f7bad0080c8c8320f24763
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 13:19:16 +0000

    [automatic commit] 2025-06-20 13:19:16 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,11 @@ router.post('/login', async (req, res) => {
     }
 
     // Save user info in session after login
-
+    req.session.user = {
+      id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
+    };
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
