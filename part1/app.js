f5a629de664a47bcdb8c7a7808742ad90de6a1e8
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 08:37:41 +0000

    [automatic commit] 2025-06-20 08:37:40 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
-const app = express();
-const port = 3000;
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql2/promise');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let db;
 
