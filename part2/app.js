dfdc51ad124d30dcfa42d7cbf53ea220d071b60e
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 13:46:18 +0000

    [automatic commit] 2025-06-20 13:46:18 UTC

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// session
 app.use(session({
     secret: process.env.SESSION_SECRET || 'password',
     resave: false,
