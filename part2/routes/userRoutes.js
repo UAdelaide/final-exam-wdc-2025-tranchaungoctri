cdc3cef260851bb9c9e15d66f9669aab92fc868a
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-20 12:45:25 +0000

    [automatic commit] 2025-06-20 12:45:24 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -14,7 +14,9 @@ router.get('/', async (req, res) => {
 
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const {
+    username, email, password, role
+  } = req.body;
 
   try {
     const [result] = await db.query(`
