5611fd9c804a8138ae7204da01f00bc8716a5a90
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-21 04:58:06 +0000

    [automatic commit] 2025-06-21 04:58:06 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -94,7 +94,7 @@ router.get('/dogs/mine', async (req, res) => {
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Dogs.owner_id, Users.username AS owner_username
       FROM Dogs
       JOIN Users ON Dogs.owner_id = Users.user_id
     `);
