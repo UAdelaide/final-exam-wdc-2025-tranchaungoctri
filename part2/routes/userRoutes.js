d4621b067b7017378fd7779ba05c382cd1ed1f5f
tranchaungoctri <tranchaungoctri@gmail.com>
2025-06-21 01:55:17 +0000

    [automatic commit] 2025-06-21 01:55:17 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,7 +58,6 @@ router.post('/login', async (req, res) => {
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
   }
 });
 
